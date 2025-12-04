e1712437269a604d05131643215b3c59a8c8b8f5
Tomas-Personal <tomasdelbo@gmail.com>
2025-12-04 19:49:08 -0300

    Interpretacion y graficos exploratorios

---
+++ 04_graficos_exploratorios.R
@@ -0,0 +1,81 @@
+# Librerias
+library(ggplot2)
+library(dplyr)
+library(car)
+
+png("output/Distribución de Supervivencia.png", width = 1000, height = 600)
+
+ggplot(titanic_clean, aes(x = factor(Survived), fill = factor(Survived))) +
+  geom_bar() +
+  geom_text(
+    stat = "count",
+    aes(label = sprintf("%.1f%%", after_stat(count / sum(count) * 100))),
+    vjust = -0.3,
+    size = 4
+  ) +
+  labs(
+    title = "Distribución de Supervivencia",
+    x = "Survived (0 = no, 1 = si)",
+    y = "Cantidad de pasajeros"
+  ) +
+  ylim(0, max(table(titanic_clean$Survived)) * 1.1) +  # espacio para los %
+  theme_minimal()
+
+
+dev.off()
+
+png("output/Proporción de supervivencia por sexo.png", width = 1000, height = 600)
+ggplot(titanic_clean, aes(x = Sex, fill = Survived)) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  labs(
+    title = "Proporción de supervivencia por sexo",
+    x = "Sexo",
+    y = "Porcentaje"
+  )
+dev.off()
+
+png("output/Edad por condición de supervivencia.png", width = 1000, height = 600)
+ggplot(titanic_clean, aes(x = Survived, y = Age, fill = Survived)) +
+  geom_boxplot() +
+  labs(
+    title = "Edad por condición de supervivencia",
+    x = "Survived (0 = no, 1 = sí)",
+    y = "Edad"
+  )
+dev.off()
+
+png("output/Relación entre Edad y Tarifa.png", width = 1000, height = 600)
+ggplot(titanic_clean, aes(x = Age, y = Fare, color = Survived)) +
+  geom_point(alpha = 0.6) +
+  labs(
+    title = "Relación entre Edad y Tarifa (color = Survived)",
+    x = "Edad",
+    y = "Tarifa pagada"
+  )
+dev.off()
+
+png("output/Supervivencia según Clase.png", width = 1000, height = 600)
+
+ggplot(titanic_clean,
+       aes(x = Pclass, fill = Survived)) +
+  geom_bar(position = "fill") +
+  geom_text(
+    stat = "count",
+    aes(
+      label = sprintf("%.1f%%", after_stat(prop * 100)),
+      y = after_stat(prop),
+      group = Survived
+    ),
+    position = position_fill(vjust = 0.5),
+    color = "white",
+    size = 4
+  ) +
+  labs(
+    title = "Supervivencia según Clase",
+    x = "Clase (1 = Alta, 2 = Media, 3 = Baja)",
+    y = "Proporción dentro de cada clase",
+    fill = "Survived"
+  ) +
+  theme_minimal()
+dev.off()
